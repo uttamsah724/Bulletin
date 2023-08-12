14e68a723bf33537aa27a0242976a5fef7876dab
Uttam Sah <uttamkumarsah111@gmail.com>
2023-08-12 16:59:16 +0530

    second

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function reload() {
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
+    console.log(data);
     bindData(data.articles);
 }
 
